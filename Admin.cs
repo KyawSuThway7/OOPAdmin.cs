96f0e65ce7da1a6bac5811f522b035c272ef9127
KyawSuThway7 <118601502+KyawSuThway7@users.noreply.github.com>
2022-12-21 18:19:01 +0630

    Add files via upload

---
+++ Admin.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentforOOP
+{
+    class Admin:IManage
+    {
+
+       public int Years { get; set; }
+        public double minutes { get; set; }
+        public void CreatePost()
+        {
+            Console.WriteLine("\t For Admin");
+            Console.WriteLine("Creation : a Poem with beautiful photo");
+            Console.WriteLine("Creation : a Oscar video ");
+            Console.WriteLine("Creation : a Other video");
+        }
+        public double ChangeCoverphoto(int years)
+        {
+            double result = (years*365)/7 * 3;
+            return result;
+
+        }
+
+
+        public  void ChangeCoverPhotoInYear(int years, double totalphoto)
+        {
+            Console.WriteLine($"You can change {totalphoto} photos in  {years} years  ");
+        }
+        public double GetMoneyFromUser(double Cost,double adveradvertisment, double AppCreater)//one minutes get 10kyats,AppCreater give 5 kyats
+        {
+            double result = (minutes * Cost)+adveradvertisment - minutes * AppCreater;
+            return result;
+        }
+        public void YouGetMoney(double minute, double money)
+        {
+            Console.WriteLine($"You get {money} Kyats in {minute} minutes");
+        }
+
+    }
+}
