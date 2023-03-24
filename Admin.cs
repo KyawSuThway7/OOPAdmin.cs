c75e0af3d115adf052e0ee8e81333e2755b4bc46
KyawSuThway7 <118601502+KyawSuThway7@users.noreply.github.com>
2023-03-24 21:55:00 +0630

    Update Admin.cs

--- Admin.cs
+++ Admin.cs
@@ -18,26 +18,27 @@ namespace AssignmentforOOP
             Console.WriteLine("Creation : a Oscar video ");
             Console.WriteLine("Creation : a Other video");
         }
-        public double ChangeCoverphoto(int years)
-        {
-            double result = (years*365)/7 * 3;
-            return result;
-
-        }
-
-
-        public  void ChangeCoverPhotoInYear(int years, double totalphoto)
-        {
-            Console.WriteLine($"You can change {totalphoto} photos in  {years} years  ");
-        }
-        public double GetMoneyFromUser(double Cost,double adveradvertisment, double AppCreater)//one minutes get 10kyats,AppCreater give 5 kyats
+        
+          public double GetMoneyFromUser(double Cost,double adveradvertisment, double AppCreater)//one minutes get 10kyats,AppCreater give 5 kyats
         {
             double result = (minutes * Cost)+adveradvertisment - minutes * AppCreater;
             return result;
         }
-        public void YouGetMoney(double minute, double money)
+        
+         public void YouGetMoney(double minute, double money)
         {
             Console.WriteLine($"You get {money} Kyats in {minute} minutes");
+        }
+        
+        public double ChangeCoverphoto(int years)
+        {
+            double result = (years*365)/7 * 3;
+            return result;
+        }
+        
+        public  void ChangeCoverPhotoInYear(int years, double totalphoto)
+        {
+            Console.WriteLine($"You can change {totalphoto} photos in  {years} years  ");
         }
 
     }
